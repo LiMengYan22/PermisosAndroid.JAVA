4930540897a510faab6bc225c0f6be32385ef3d2
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-19 13:27:17 +0100

    Create EnManifest.java

---
+++ EnManifest.java
@@ -0,0 +1,34 @@
+<manifest ... >
+
+////////conectarse a Internet y poder descargar el contenido HTML:
+<uses-permission android:name="android.permission.INTERNET"/>
+
+////////bola EXTRA:
+android.permission.INTERNET
+android.permission.BLUETOOTH
+android.permission.BLUETOOTH_ADMIN
+android.permission.BLUETOOTH_PRIVILEGED
+android.permission.ACCESS_WIFI_STATE
+android.permission.CHANGE_WIFI_STATE
+android.permission.READ_EXTERNAL_STORAGE
+android.permission.WRITE_EXTERNAL_STORAGE
+com.offsec.nhterm.permission.RUN_SCRIPT
+com.offsec.nhterm.permission.RUN_SCRIPT_SU
+com.offsec.nhterm.permission.RUN_SCRIPT_NH
+com.offsec.nhterm.permission.RUN_SCRIPT_NH_LOGIN
+android.permission.RECEIVE_BOOT_COMPLETED
+android.permission.WAKE_LOCK
+android.permission.VIBRATE
+android.permission.ACCESS_FINE_LOCATION
+android.permission.ACCESS_COARSE_LOCATION
+android.permission.ACCESS_BACKGROUND_LOCATION
+android.permission.FOREGROUND_SERVICE
+android.permission.BIND_ACCESSIBILITY_SERVICE
+android.permission.ACCESS_NETWORK_STATE
+android.permission.REQUEST_INSTALL_PACKAGES
+android.permission.REQUEST_DELETE_PACKAGES
+android.permission.WRITE_EXTERNAL_STORAGE
+android.permission.USE_BIOMETRIC
+android.permission.USE_FINGERPRINT
+
+</manifest>
